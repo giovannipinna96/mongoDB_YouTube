99a294b22d9fa57a9ce903c36ac8c9fdeca0b930
Giovanni Pinna <giovanni.pinna.17l96@gmail.com>
2022-05-17 17:29:56 +0200

    utils function for manage the data and clean it before inset

---
+++ managecsv.py
@@ -0,0 +1,92 @@
+import pandas as pd
+
+
+def load_data(path_video: str = './trending_youtube_video_statistics_and_comments/USvideos.csv',
+              path_comments: str = './trending_youtube_video_statistics_and_comments/UScomments.csv',
+              path_category: str = './trending_youtube_video_statistics_and_comments/US_category_id.json'):
+    data_video = pd.read_csv(path_video, error_bad_lines=False)
+    data_comments = pd.read_csv(path_comments, error_bad_lines=False)
+    data_category = pd.read_json(path_category)
+
+    return data_video, data_comments, data_category
+
+
+def clean_comments(data_comments):
+    return data_comments.drop('replies', axis=1)
+
+
+def clean_category(data_category):
+    dict_category = {}
+    for i in range(len(data_category)):
+        dict_category.update({data_category['items'].iloc[i]['id']: data_category['items'].iloc[i]['snippet']['title']})
+
+    return dict_category
+
+
+def create_tags(data_video):
+    tags = data_video.copy()
+    tags.drop(['category_id', 'views', 'likes', 'dislikes', 'comment_total', 'thumbnail_link', 'date'], inplace=True,
+              axis=1)
+    tags.tags = tags.tags.apply(lambda x: x.split('|'))
+    d = {}
+    for idx, ts in enumerate(tags['tags']):
+        for t in ts:
+            if t in d:
+                d[t].append(list(tags[['video_id', 'title', 'channel_title']].iloc[idx]))
+            else:
+                temp = [tags['video_id'].iloc[idx], tags['title'].iloc[idx], tags['channel_title'].iloc[idx]]
+                d.update({t: temp})
+
+    return d
+
+
+def num_to_category(data_video, dict_category):
+    for i in range(len(data_video.category_id)):
+        if str(data_video.category_id[i]) in dict_category:
+            data_video.category_id[i] = dict_category.get(str(data_video.category_id[i]))
+        else:
+            data_video.category_id[i] = None
+
+    return data_video
+
+
+def calculate_mean_like(data_video):
+    for author in data_video['channel_title']:
+        data_video.mean_like[data_video['channel_title'] == author] = data_video.mean_like[
+            data_video['channel_title'] == author].fillna(
+            round(data_video.likes[data_video['channel_title'] == author].mean()))
+    return data_video
+
+
+def calculate_mean_dislike(data_video):
+    for author in data_video['channel_title']:
+        data_video.mean_dislike[data_video['channel_title'] == author] = data_video.mean_dislike[
+            data_video['channel_title'] == author].fillna(
+            round(data_video.dislikes[data_video['channel_title'] == author].mean()))
+
+
+def calculate_mean_view(data_video):
+    for author in data_video['channel_title']:
+        data_video.mean_view[data_video['channel_title'] == author] = data_video.mean_view[
+            data_video['channel_title'] == author].fillna(
+            round(data_video.viewa[data_video['channel_title'] == author].mean()))
+
+
+def calculate_mean_comments(data_video):
+    for author in data_video['channel_title']:
+        data_video.mean_comment[data_video['channel_title'] == author] = data_video.mean_comment[
+            data_video['channel_title'] == author].fillna(
+            round(data_video.comment_total[data_video['channel_title'] == author].mean()))
+
+
+def create_analytics_col(data_video):
+    data_video['mean_like'] = None
+    data_video['mean_dislike'] = None
+    data_video['mean_view'] = None
+    data_video['mean_comments'] = None
+
+    return data_video
+
+
+def drop_duplicates_video(data):
+    return data.drop_duplicates(subset=['video_id'], keep='last', inplace=True)
